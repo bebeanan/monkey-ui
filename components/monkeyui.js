6fa58608da15fff73a6e791cbcb0d8377a911658
xjouyi <xjouyi@163.com>
2016-06-28 15:45:48 +0800

    change index file

---
+++ components/monkeyui.js
@@ -0,0 +1,8 @@
+import Row from "./layout/row.jsx";
+import Col from "./layout/col.jsx";
+
+var MonkeyUi = {
+	Row : Row,
+	Col : Col
+}
+export default MonkeyUi;
--- monkeyui.js
+++ monkeyui.js
@@ -1,8 +1,3 @@
-import Row from "./components/layout/row.jsx";
-import Col from "./components/layout/col.jsx";
+'use strict';
 
-var MonkeyUi = {
-	Row : Row,
-	Col : Col
-}
-export default MonkeyUi;
+module.exports = require('./components/monkeyui');
