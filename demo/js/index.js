7c189bbc150c463fc36b9e07101a73e44de3ee12
xjouyi <xjouyi@163.com>
2016-09-13 09:10:57 +0800

    tree

--- demo/js/index.js
+++ demo/js/index.js
@@ -8,6 +8,7 @@ var Col = MonkeyUi.Col;
 var Button = MonkeyUi.Button;
 var CheckBox = MonkeyUi.CheckBox;
 var Modal =MonkeyUi.Modal;
+var Tree = MonkeyUi.Tree;
 
 const confirm = Modal.confirm;
 const DemoBox = props => <p className={`height-${props.value}`}>{props.children}</p>;
@@ -88,5 +89,50 @@ ReactDOM.render(
 });
 
 ReactDOM.render(<App />, document.getElementById('modal2'));
+
+
+const Demo = React.createClass({
+  getDefaultProps() {
+    return {
+      keys: ['0-0-0', '0-0-1'],
+    };
+  },
+  getInitialState() {
+    const keys = this.props.keys;
+    return {
+      defaultExpandedKeys: keys,
+      defaultSelectedKeys: keys,
+      defaultCheckedKeys: keys,
+    };
+  },
+  onSelect(info) {
+    console.log('selected', info);
+  },
+  onCheck(info) {
+    console.log('onCheck', info);
+  },
+  render() {
+    return (
+      <Tree className="myCls" showLine checkable
+        defaultExpandedKeys={this.state.defaultExpandedKeys}
+        defaultSelectedKeys={this.state.defaultSelectedKeys}
+        defaultCheckedKeys={this.state.defaultCheckedKeys}
+        onSelect={this.onSelect} onCheck={this.onCheck}
+      >
+        <TreeNode title="parent 1" key="0-0">
+          <TreeNode title="parent 1-0" key="0-0-0" disabled>
+            <TreeNode title="leaf" key="0-0-0-0" disableCheckbox />
+            <TreeNode title="leaf" key="0-0-0-1" />
+          </TreeNode>
+          <TreeNode title="parent 1-1" key="0-0-1">
+            <TreeNode title={<span style={{ color: '#08c' }}>sss</span>} key="0-0-1-0" />
+          </TreeNode>
+        </TreeNode>
+      </Tree>
+    );
+  },
+});
+
+ReactDOM.render(<Demo />, document.getElementById('tree'));
  
  
