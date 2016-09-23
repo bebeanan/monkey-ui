4b66f0dd73a69ca96399ace74c563ebcd3b7a930
banana <banana>
2016-09-23 15:07:13 +0800

    'demo'

--- demo/js/Page.js
+++ demo/js/Page.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import MonkeyUi from '../../components/monkeyui.js';
-import MonkeyUi from '../../monkeyui.js';
+import MonkeyUi from '../../lib/monkeyui.js';
 var Collapse=MonkeyUi.Collapse;
+const Panel=Collapse.Panel;
 const Page = React.createClass({
   render() {
     return (
-  <Collapse defaultActiveKey={['1']} onChange={callback}>
+  <Collapse >
     <Panel header="This is panel header 1" key="1">
-      <p>{text}</p>
+      <p>123</p>
     </Panel>
     <Panel header="This is panel header 2" key="2">
-      <p>{text}</p>
+      <p>456</p>
     </Panel>
     <Panel header="This is panel header 3" key="3">
-      <p>{text}</p>
+      <p>567</p>
     </Panel>
   </Collapse>
     );
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,35 +1,27 @@
 import gulp from 'gulp';
-import gulpLoadPlugins from  'gulp-load-plugins';
 import babel from 'gulp-babel';
 import less from 'gulp-less';
 import concat from 'gulp-concat';
 import cssmin from 'gulp-minify-css';
 import gutil from "gulp-util";
-import webpack from 'webpack';
+import webpack from 'gulp-webpack';
+import uglify from 'gulp-uglify';
+import minifyHtml from 'gulp-htmlmin'
 import browserSync from 'browser-sync';
 import webpackConfig from './webpack.config.js';
 import WebpackDevServer from 'webpack-dev-server';
+import  HtmlWebpackPlugin from 'html-webpack-plugin';
 const dirs={
+	demosrc : ['./demo/js/index.js'],
 	src : ['./components/**/*.js','./components/**/*.jsx'],
 	less :[	'./components/style/index.less',
 			'./components/**/style/index.less',
-			// './components/layout/style/index.less',
-			// './components/modal/style/index.less',
-			// './components/tree/style/index.less',
-			// './components/message/style/index.less',
-			// './components/alert/style/index.less',
-			// './components/collapse/style/index.less',
-			// './components/input/style/index.less'
 			],
 	lib: 'lib',
+	demolib: 'showdemo/js',
 	dist :'lib/dist'
 }
 
-gulp.task('webpack',() =>{
-	webpack(webpackConfig,function (callback) {
-		
-	});
-});
 
 gulp.task('less',() =>{
 	return gulp.src(dirs.less)
@@ -37,6 +29,7 @@ gulp.task('less',() =>{
 		.pipe(concat('monkeyui.css'))
 		.pipe(cssmin())
 		.pipe(gulp.dest(dirs.dist))
+		.pipe(gulp.dest('./showdemo/css'))
 });
 
 gulp.task('babel',() => {
@@ -44,11 +37,50 @@ gulp.task('babel',() => {
 		.pipe(babel())
 		.pipe(gulp.dest(dirs.lib))
 });
-
+// 压缩 HTML
+gulp.task('html', () => {
+  return gulp.src('./demo/templates/index.html')
+    .pipe(minifyHtml())
+    .pipe(gulp.dest('./showdemo'))
+});
+//压缩js
+gulp.task('demojs',() => {
+	return gulp.src(dirs.demosrc)
+		   .pipe(webpack({
+		      watch: false,
+		      output: {
+		        filename: 'index.js',
+		        comments: false,  // remove all comments
+		      },
+		      module: {
+		        loaders: [
+		          {
+		            test: /\.js$/,
+		            exclude: /node_modules/,
+		            loader: 'babel'
+		          }
+		        ]
+      		},
+		    }))
+		.pipe(gulp.dest(dirs.demolib))
+		/*ss.pipe(uglify(
+	        {
+	            mangle: true,//类型：Boolean 默认：true 是否修改变量名
+	            compress: true,//类型：Boolean 默认：true 是否完全压缩
+	            //preserveComments: 'all' //保留所有注释
+	            comments: false
+	        }
+	      ))
+	    .pipe(rename({suffix: '.min'}))
+	    .pipe(gulp.dest(dirs.demolib))
+	    .pipe(size({
+	      title: 'script minify'
+	    }));*/
+});
 
 
 gulp.task('build',()=>{
-	gulp.start('babel', 'less');
+	gulp.start('babel', 'less','demojs','html');
 });
 
 // 监控修改自动编译
@@ -56,20 +88,19 @@ gulp.task('watch', () => {
   gulp.watch('./components/**/style/*.less', ['build']);
   gulp.watch('./components/**/*.js', ['build']);
   gulp.watch('./components/**/*.jsx', ['build']);
-  gulp.watch('./demo/**/*.js', ['build']);
   gulp.watch('./demo/js/**/*.js', ['build']);
-  gulp.watch('./demo/**/*.html', ['build']);
+  gulp.watch('./demo/templates/**/*.html', ['build']);
 });
 
 // 启动预览服务自动刷新浏览器
 gulp.task('default', ['build', 'watch'], () => {
- /* const bs = browserSync.create();
+  const bs = browserSync.create();
 
   bs.init({
     notify: false,
     logPrefix: 'AMT',
-    server: 'demo/templates/'
+    server: 'showdemo/'
   });
   gulp.watch('components', bs.reload);
-  gulp.watch('demo', bs.reload);*/
+  gulp.watch('demo', bs.reload);
 });
